62d29ad48ecf06796588cf7a528d73fc43740641
zsoltkovacs94 <zsoltkovacs94@mailbox.unideb.hu>
2023-11-03 18:30:11 +0100

    Oldalnavigáció javítva.

--- koffein_flu_prediction/website/urls.py
+++ koffein_flu_prediction/website/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name="home")
+    path('', views.home, name="home"),
+    path('/gen', views.gen, name="gen"),
+    path('/tan', views.tan, name="tan")
 ]
--- koffein_flu_prediction/website/views.py
+++ koffein_flu_prediction/website/views.py
@@ -2,3 +2,9 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'index.html', {})
+
+def gen(request):
+    return render(request, 'index.html', {})
+
+def tan(request):
+    return render(request, 'tanulo.html', {})
