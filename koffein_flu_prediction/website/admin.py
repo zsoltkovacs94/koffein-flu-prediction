36a3c77351af2d96bf65cd570b9bed38b5ec3190
Jurás Dorka <ijustcantjudo@gmail.com>
2023-11-03 14:58:10 +0100

    models.py frissítése a lekert_adatok és generalt_adatok táblákkal (még adatok nélkül)

--- koffein_flu_prediction/website/admin.py
+++ koffein_flu_prediction/website/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import lekert_adatok
+from .models import generalt_adatok
+
+admin.site.register(lekert_adatok)
+admin.site.register(generalt_adatok)
 
 # Register your models here.
--- koffein_flu_prediction/website/models.py
+++ koffein_flu_prediction/website/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+
+
+class lekert_adatok (models.Model):
+    WHOREGION = models.CharField("WHOREGION", max_length=10)
+    COUNTRY_AREA_TERRITORY = models.CharField("COUNTRY_AREA_TERRITORY", max_length=255)
+    ISO_YEAR = models.IntegerField("ISO_YEAR")
+    ISO_WEEK = models.IntegerField("ISO_WEEK")
+    ILI_CASE = models.IntegerField("ILI_CASE")
+    ILI_OUTPATIENTS = models.IntegerField("ILI_OUTPATIENTS")
+    SARI_CASE = models.IntegerField("SARI_CASE")
+    SARI_INPATIENTS = models.IntegerField("SARI_INPATIENTS")
+
+
+class generalt_adatok(models.Model):
+    WHOREGION = models.CharField("WHOREGION", max_length=10)
+    COUNTRY_AREA_TERRITORY = models.CharField("COUNTRY_AREA_TERRITORY", max_length=255)
+    ISO_YEAR = models.IntegerField("ISO_YEAR")
+    ISO_WEEK = models.IntegerField("ISO_WEEK")
+    ILI_CASE = models.IntegerField("ILI_CASE")
+    ILI_OUTPATIENTS = models.IntegerField("ILI_OUTPATIENTS")
+    SARI_CASE = models.IntegerField("SARI_CASE")
+    SARI_INPATIENTS = models.IntegerField("SARI_INPATIENTS")
+
