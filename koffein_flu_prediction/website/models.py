ffac8f00395b17bf5b548842615f85e3f254c465
Jurás Dorka <ijustcantjudo@gmail.com>
2023-11-03 16:44:02 +0100

    Lekért adatok tábla feltöltése az updated_learning_data.csv fájl adataival

--- koffein_flu_prediction/website/models.py
+++ koffein_flu_prediction/website/models.py
@@ -5,22 +5,22 @@ from django.db import models
 
 class lekert_adatok(models.Model):
     WHOREGION = models.CharField("WHOREGION", max_length=10)
-    COUNTRY_AREA_TERRITORY = models.CharField("COUNTRY_AREA_TERRITORY", max_length=255)
+    COUNTRY_AREA_TERRITORY = models.TextField("COUNTRY_AREA_TERRITORY", max_length=255)
     ISO_YEAR = models.IntegerField("ISO_YEAR")
     ISO_WEEK = models.IntegerField("ISO_WEEK")
-    ILI_CASE = models.IntegerField("ILI_CASE")
-    ILI_OUTPATIENTS = models.IntegerField("ILI_OUTPATIENTS")
-    SARI_CASE = models.IntegerField("SARI_CASE")
-    SARI_INPATIENTS = models.IntegerField("SARI_INPATIENTS")
+    ILI_CASE = models.IntegerField("ILI_CASE", blank=True, null=True)
+    ILI_OUTPATIENTS = models.IntegerField("ILI_OUTPATIENTS", blank=True, null=True)
+    SARI_CASE = models.IntegerField("SARI_CASE", blank=True, null=True)
+    SARI_INPATIENTS = models.IntegerField("SARI_INPATIENTS", blank=True, null=True)
 
 
 class generalt_adatok(models.Model):
     WHOREGION = models.CharField("WHOREGION", max_length=10)
-    COUNTRY_AREA_TERRITORY = models.CharField("COUNTRY_AREA_TERRITORY", max_length=255)
+    COUNTRY_AREA_TERRITORY = models.TextField("COUNTRY_AREA_TERRITORY", max_length=255)
     ISO_YEAR = models.IntegerField("ISO_YEAR")
     ISO_WEEK = models.IntegerField("ISO_WEEK")
-    ILI_CASE = models.IntegerField("ILI_CASE")
-    ILI_OUTPATIENTS = models.IntegerField("ILI_OUTPATIENTS")
-    SARI_CASE = models.IntegerField("SARI_CASE")
-    SARI_INPATIENTS = models.IntegerField("SARI_INPATIENTS")
+    ILI_CASE = models.IntegerField("ILI_CASE", blank=True, null=True)
+    ILI_OUTPATIENTS = models.IntegerField("ILI_OUTPATIENTS", blank=True, null=True)
+    SARI_CASE = models.IntegerField("SARI_CASE", blank=True, null=True)
+    SARI_INPATIENTS = models.IntegerField("SARI_INPATIENTS", blank=True, null=True)
 
