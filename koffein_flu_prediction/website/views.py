db83c98956d16858f3c4bc32ad555754b42a554c
zsoltkovacs94 <zsoltkovacs94@mailbox.unideb.hu>
2023-11-05 13:33:30 +0100

    Javítás, hogy a tanuló adatbázis oldalán jelenjenek meg a tanuló adatok.

--- koffein_flu_prediction/website/views.py
+++ koffein_flu_prediction/website/views.py
@@ -2,11 +2,11 @@ from django.shortcuts import render
 from .models import lekert_adatok
 
 def home(request):
-    database = lekert_adatok.objects.all()[:1000]
-    return render(request, 'index.html', {'all': database})
+    return render(request, 'index.html', {})
 
 def gen(request):
     return render(request, 'index.html', {})
 
 def tan(request):
-    return render(request, 'tanulo.html', {})
+    database = lekert_adatok.objects.all()[:1000]
+    return render(request, 'tanulo.html', {'all': database})
