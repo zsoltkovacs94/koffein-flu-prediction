a7d4c626493a550d2afd647b41476535b05822f7
Jurás Dorka <ijustcantjudo@gmail.com>
2023-11-10 14:43:30 +0100

    models.py - lekert_adatok tábla lekérdezéseinek megírása

--- koffein_flu_prediction/website/models.py
+++ koffein_flu_prediction/website/models.py
@@ -24,3 +24,37 @@ class generalt_adatok(models.Model):
     SARI_CASE = models.IntegerField("SARI_CASE", blank=True, null=True)
     SARI_INPATIENTS = models.IntegerField("SARI_INPATIENTS", blank=True, null=True)
 
+
+def getAllData():
+    allData = lekert_adatok.objects.all()
+    return allData
+
+
+def filterByWHORegion(whoRegion):
+    filteredWHORegion = lekert_adatok.objects.filter(WHOREGION=whoRegion)
+    return filteredWHORegion
+
+
+def filterByCoarte(coarte):
+    filteredCoarte = lekert_adatok.objects.filter(COUNTRY_AREA_TERRITORY=coarte)
+    return filteredCoarte
+
+
+def filterByYear(isoYear):
+    filteredYear = lekert_adatok.objects.filter(ISO_YEAR=isoYear)
+    return filteredYear
+
+
+def filterByTime(isoYear, isoWeek):
+    filteredTime = lekert_adatok.objects.filter(ISO_YEAR=isoYear, ISO_WEEK=isoWeek)
+    return filteredTime
+
+
+def filterByWHORegionAndTime(whoRegion, isoYear, isoWeek):
+    filteredWHOTime = lekert_adatok.objects.filter(WHOREGION=whoRegion, ISO_YEAR=isoYear, ISO_WEEK=isoWeek)
+    return filteredWHOTime
+
+
+def filterByCoarteAndTime(coarte, isoYear, isoWeek):
+    filteredCoarteTime = lekert_adatok.objects.filter(COUNTRY_AREA_TERRITORY=coarte, ISO_YEAR=isoYear, ISO_WEEK=isoWeek)
+    return filteredCoarteTime
