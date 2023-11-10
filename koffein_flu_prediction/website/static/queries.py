2fd79e0b7a2d5b591403981efbcb63983df75b32
Jurás Dorka <ijustcantjudo@gmail.com>
2023-11-10 18:27:25 +0100

    models.py lekérdezések eltávolítása, queries.py lekérdezések megírása mindkét táblára

--- koffein_flu_prediction/website/static/queries.py
+++ koffein_flu_prediction/website/static/queries.py
@@ -2,50 +2,83 @@ from ..models import lekert_adatok
 from ..models import generalt_adatok
 from .. import urls
 
+
 def getAllData(path):
-    if(path == urls.urlpatterns[0] | path == urls.urlpatterns[2]):
+    if (path == urls.urlpatterns[0] or path == urls.urlpatterns[2]):
         allData = lekert_adatok.objects.all()
-    elif(path == urls.urlpatterns[1]):
+    elif (path == urls.urlpatterns[1]):
         allData = generalt_adatok.objects.all()
 
     return allData
 
 
-def filterByWHORegion(whoRegion):
-    filteredWHORegion = lekert_adatok.objects.filter(WHOREGION=whoRegion)
+def filterByWHORegion(path, whoRegion):
+    if (path == urls.urlpatterns[0] or path == urls.urlpatterns[2]):
+        filteredWHORegion = lekert_adatok.objects.filter(WHOREGION=whoRegion)
+    elif (path == urls.urlpatterns[1]):
+        filteredWHORegion = generalt_adatok.objects.filter(WHOREGION=whoRegion)
+
     return filteredWHORegion
 
 
-def filterByCoarte(coarte):
-    filteredCoarte = lekert_adatok.objects.filter(COUNTRY_AREA_TERRITORY=coarte)
+def filterByCoarte(path, coarte):
+    if (path == urls.urlpatterns[0] or path == urls.urlpatterns[2]):
+        filteredCoarte = lekert_adatok.objects.filter(COUNTRY_AREA_TERRITORY=coarte)
+    elif (path == urls.urlpatterns[1]):
+        filteredCoarte = generalt_adatok.objects.filter(COUNTRY_AREA_TERRITORY=coarte)
+
     return filteredCoarte
 
 
-def filterByYear(isoYear):
-    filteredYear = lekert_adatok.objects.filter(ISO_YEAR=isoYear)
+def filterByYear(path, isoYear):
+    if (path == urls.urlpatterns[0] or path == urls.urlpatterns[2]):
+        filteredYear = lekert_adatok.objects.filter(ISO_YEAR=isoYear)
+    elif (path == urls.urlpatterns[1]):
+        filteredYear = generalt_adatok.objects.filter(ISO_YEAR=isoYear)
+
     return filteredYear
 
 
-def filterByTime(isoYear, isoWeek):
-    filteredTime = lekert_adatok.objects.filter(ISO_YEAR=isoYear, ISO_WEEK=isoWeek)
+def filterByTime(path, isoYear, isoWeek):
+    if (path == urls.urlpatterns[0] or path == urls.urlpatterns[2]):
+        filteredTime = lekert_adatok.objects.filter(ISO_YEAR=isoYear, ISO_WEEK=isoWeek)
+    elif (path == urls.urlpatterns[1]):
+        filteredTime = generalt_adatok.objects.filter(ISO_YEAR=isoYear, ISO_WEEL=isoWeek)
+
     return filteredTime
 
 
-def filterByWHORegionAndYear(whoRegion, isoYear):
-    filteredWHOYear = lekert_adatok.objects.filter(WHOREGION=whoRegion, ISO_YEAR=isoYear)
+def filterByWHORegionAndYear(path, whoRegion, isoYear):
+    if (path == urls.urlpatterns[0] or path == urls.urlpatterns[2]):
+        filteredWHOYear = lekert_adatok.objects.filter(WHOREGION=whoRegion, ISO_YEAR=isoYear)
+    elif (path == urls.urlpatterns[1]):
+        filteredWHOYear = generalt_adatok.objects.filter(WHOREGION=whoRegion, ISO_YEAR=isoYear)
+
     return filteredWHOYear
 
 
-def filterByWHORegionAndTime(whoRegion, isoYear, isoWeek):
-    filteredWHOTime = lekert_adatok.objects.filter(WHOREGION=whoRegion, ISO_YEAR=isoYear, ISO_WEEK=isoWeek)
+def filterByWHORegionAndTime(path, whoRegion, isoYear, isoWeek):
+    if (path == urls.urlpatterns[0] or path == urls.urlpatterns[2]):
+        filteredWHOTime = lekert_adatok.objects.filter(WHOREGION=whoRegion, ISO_YEAR=isoYear, ISO_WEEK=isoWeek)
+    elif (path == urls.urlpatterns[1]):
+        filteredWHOTime = generalt_adatok.objects.filter(WHOREGION=whoRegion, ISO_YEAR=isoYear, ISO_WEEK=isoWeek)
+
     return filteredWHOTime
 
 
-def filterByCoarteAndYear(coarte, isoYear):
-    filteredCoarteYear = lekert_adatok.objects.filter(COUNTRY_AREA_TERRITORY=coarte, ISO_YEAR=isoYear)
+def filterByCoarteAndYear(path, coarte, isoYear):
+    if (path == urls.urlpatterns[0] or path == urls.urlpatterns[2]):
+        filteredCoarteYear = lekert_adatok.objects.filter(COUNTRY_AREA_TERRITORY=coarte, ISO_YEAR=isoYear)
+    elif (path == urls.urlpatterns[1]):
+        filteredCoarteYear = generalt_adatok.objects.filter(COUNTRY_AREA_TERRITORY=coarte, ISO_YEAR=isoYear)
+
     return filteredCoarteYear
 
 
-def filterByCoarteAndTime(coarte, isoYear, isoWeek):
-    filteredCoarteTime = lekert_adatok.objects.filter(COUNTRY_AREA_TERRITORY=coarte, ISO_YEAR=isoYear, ISO_WEEK=isoWeek)
+def filterByCoarteAndTime(path, coarte, isoYear, isoWeek):
+    if (path == urls.urlpatterns[0] or path == urls.urlpatterns[2]):
+        filteredCoarteTime = lekert_adatok.objects.filter(COUNTRY_AREA_TERRITORY=coarte, ISO_YEAR=isoYear, ISO_WEEK=isoWeek)
+    elif (path == urls.urlpatterns[1]):
+        filteredCoarteTime = generalt_adatok.objects.filter(COUNTRY_AREA_TERRITORY=coarte, ISO_YEAR=isoYear, ISO_WEEK=isoWeek)
+
     return filteredCoarteTime
