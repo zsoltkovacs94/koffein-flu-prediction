64ce16962ac64e26206ede902911cb767e50c99b
Jurás Dorka <ijustcantjudo@gmail.com>
2023-11-11 09:36:26 +0100

    generált adatok szűrőjének hozzáadása a Tanuló adatok szűrője alapján

--- koffein_flu_prediction/website/models.py
+++ koffein_flu_prediction/website/models.py
@@ -42,3 +42,20 @@ class generalt_adatok(models.Model):
     ILI_OUTPATIENTS = models.IntegerField("ILI_OUTPATIENTS", blank=True, null=True)
     SARI_CASE = models.IntegerField("SARI_CASE", blank=True, null=True)
     SARI_INPATIENTS = models.IntegerField("SARI_INPATIENTS", blank=True, null=True)
+
+    def genFilter(self, WHR = "Any", coarte = "Any", startDate = "1996-01-01", endDate = datetime.now().strftime("%Y-%m-%d")):
+        data = generalt_adatok.objects.all()
+        if(WHR != "Any"):
+            data = data.filter(WHOREGION=WHR)
+        if(coarte != "Any"):
+            data = data.filter(COUNTRY_AREA_TERRITORY=coarte)
+        sDate = datetime.strptime(startDate, '%Y-%m-%d')
+        sYear = int(sDate.year)
+        sWeek = sDate.isocalendar().week
+        eDate = datetime.strptime(endDate, '%Y-%m-%d')
+        eYear = eDate.year
+        eWeek = eDate.isocalendar().week
+        f1 = Q(ISO_YEAR=sYear)
+        f2 = Q(ISO_WEEK__gte=sWeek)
+        f3 = Q(ISO_YEAR__gt=sYear)
+        return data.filter((f1 & f2) | f3).order_by("ISO_WEEK").order_by("ISO_YEAR")
