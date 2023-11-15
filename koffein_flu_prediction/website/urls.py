a5d15385e0fc2cf18cebc70a39c8ae2fc7ef02a7
MadarBoglarka <144042076+MadarBoglarka@users.noreply.github.com>
2023-11-15 13:48:24 +0100

    gen oldal hozza adása

--- koffein_flu_prediction/website/urls.py
+++ koffein_flu_prediction/website/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name="home"),
     path('gen', views.gen, name="gen"),
+    path('index', views.index, name="index"),
     path('tan', views.tan, name="tan")
+
 ]
