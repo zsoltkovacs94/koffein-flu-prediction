83f9010a4b2503d6a0735440acc8e7447613ca95
zsoltkovacs94 <zsoltkovacs94@mailbox.unideb.hu>
2023-11-25 10:47:10 +0100

    favicon.ico pótlása

--- koffein_flu_prediction/koffein_flu_prediction/urls.py
+++ koffein_flu_prediction/koffein_flu_prediction/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
+from django.contrib.staticfiles.storage import staticfiles_storage
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('website.urls'))
+    path('', include('website.urls')),
+	path('favicon.ico', RedirectView.as_view(url=staticfiles_storage.url('img/favicon.ico')))
 ]
